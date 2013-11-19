5235a0fb49d9a2c27359c8ead15f8e4d145cd2f8
Jan-Henrik Bruhn <jh.bruhn@me.com>
2013-11-19 15:12:37 +0100

    added first tests for pictures

--- test/picture.js
+++ test/picture.js
@@ -8,6 +8,21 @@ var puutWithoutAuth = common.puutWithoutAuth;
 
 describe('Pictures', function() {
   describe('POST /', function() {
+    it('should fail without auth', function(done) {
+      request(puutWithAuth)
+        .get('/')
+        .set('Accept', 'text/html')
+        .expect('Content-Type', /html/)
+        .expect(401, done);
+    });
     
+    it('should fail with wrong auth', function(done) {
+      request(puutWithAuth)
+        .get('/')
+        .set('Accept', 'text/html')
+        .auth('ralf', 'dietmar')
+        .expect('Content-Type', /html/)
+        .expect(401, done);
+    });
   })
 });
