37624fd276689cad87efe43aaa322956643f4476
Sören Gade <gade.soeren@googlemail.com>
2013-11-24 23:03:42 +0100

    Added ‘grunt test’ command running mocha tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,22 @@ module.exports = function(grunt) {
     jshint: {
       server: [ '*.js', 'routes/**/*.js' ],
       tests: [ 'test/**/*.js' ]
+    },
+    
+    mochaTest: {
+      all: {
+        options: {
+          reporter: 'spec'
+        },
+        src: [ 'test/*.js' ]
+      }
     }
   });
   
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
   
   grunt.registerTask('default', [ 'jshint' ]);
+  grunt.registerTask('test', [ 'jshint', 'mochaTest' ]);
   
 };
